58a96ace1c412b6a7f4b979a76a72fbec784fb03
Neil Satterfield <neilsatt@gmail.com>
2017-06-06 21:42:09 -0600

    more responsive cleanup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 let alertBtn = document.getElementById('alertButton');
 alertBtn.addEventListener('click', removeAlert, false);
+alertBtn.addEventListener('click', removeCircle, false);
 
 function removeAlert() {
     let alertDiv = document.getElementById('alertMessage');
     alertDiv.parentNode.removeChild(alertDiv);
+}
+
+function removeCircle() {
+    let bellCircle = document.getElementById('circle');
+    bellCircle.parentNode.removeChild(bellCircle);
 }
