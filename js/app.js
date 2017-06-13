bbf38c23765706bf7a24800bbf47d90bdf5c5a10
Neil Satterfield <neilsatt@gmail.com>
2017-06-12 19:06:53 -0600

    add jquery library, initial save btn commit

--- js/app.js
+++ js/app.js
@@ -11,3 +11,31 @@ function removeCircle() {
     let bellCircle = document.getElementById('circle');
     bellCircle.parentNode.removeChild(bellCircle);
 }
+
+// send btn functionality
+let sendBtn = document.getElementById('sendBtn');
+sendBtn.addEventListener('click', sendMessage, false);
+
+function sendMessage() {
+    alert('hi there');
+    //if user isn’t selected or message field is empty.
+}
+
+// jquery on/off button
+new DG.OnOffSwitch({
+    el: '#on-off-switch',
+    textOn: 'On',
+    textOff: 'Off',
+    width: 90,
+    trackColorOn:'#716ecc',
+
+});
+
+new DG.OnOffSwitch({
+    el: '#on-off-switch2',
+    textOn: 'On',
+    textOff: 'Off',
+    width: 90,
+    trackColorOn:'#716ecc',
+
+});
